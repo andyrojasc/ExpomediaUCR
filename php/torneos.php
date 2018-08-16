12c6a208d711fb8a7ba313065c8d42a30fa13d60
CHELVIVIL96 <keylor139@gmail.com>
2018-08-16 13:19:45 -0600

    commit 13:19
    detalles finales bluir

--- php/torneos.php
+++ php/torneos.php
@@ -13,16 +13,18 @@
 </head>
 <body class="BodyBg">
   <!-- inicio header -->
+
   <header>
     <!-- navbar -->
  <?php @include('../includes/menu2.php'); ?>
       <!-- navbar -->
-      <div id="contenido-pagina">
-      <div class="container-fluid">
+    
+      <div id="contenido-pagina2" class="container-fluid">
         <div class="row">
           <div class="imgHeader col-xs-12 col-sm-12 col-md-12 col-lg-12 img-fluid"><img src="../assets/images/torneoHeader.png" alt="Ganadores-pasados-del-torneo-de-League-of-Legends"></div>
         </div>
       </div>
+    
     </header>
     <!-- fin del header -->
 <div id="contenido-pagina">
